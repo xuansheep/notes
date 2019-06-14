9edbff0d1a8d200302975dcdbcfda9a6758a4e44
xuansheep <xuansheep@gmail.com>
2019-06-14 18:08:34 +0800

    项目开发

--- src/components/http.js
+++ src/components/http.js
@@ -4,7 +4,7 @@ import {Message} from 'iview'
 import 'iview/dist/styles/iview.css'
 
 
-const TIME_OUT_MS = 60 * 1000 // 默认请求超时时间
+const TIME_OUT_MS = 60 * 1000; // 默认请求超时时间
 
 /*
  * @param response 返回数据列表
@@ -15,7 +15,7 @@ function handleResults (response) {
 }
 
 function handleUrl (url) {
-    url = 'http://localhost:8883'+url;
+    url = 'http://localhost:10001'+url;
 // BASE_URL是接口的ip前缀，比如http:10.100.1.1:8989/
     return url
 }
