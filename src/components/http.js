beb61d3fbbdd74e10e5702d7850dd74616df7630
xuansheep <xuansheep@gmail.com>
2019-07-24 19:34:11 +0800

    项目开发

--- src/components/http.js
+++ src/components/http.js
@@ -15,8 +15,7 @@ function handleResults (response) {
 }
 
 function handleUrl (url) {
-    // url = 'http://127.0.0.1:9001'+url;
-    url = 'http://47.106.79.168:9001'+url;
+    url = 'http://47.106.79.168:10001'+url;
 
 // BASE_URL是接口的ip前缀，比如http:10.100.1.1:8989/
     return url
@@ -189,7 +188,8 @@ export default {
             data: handleParamsUpload(data),
             timeout: TIME_OUT_MS,
             headers: {
-                'Content-Type': 'multipart/form-data; boundary='+123
+                'Content-Type': 'multipart/form-data; boundary='+123,
+                'token': getToken(),
             }
         }).then(
             (result) => {
