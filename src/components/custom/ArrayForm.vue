4953dc33489070ecb4fc559b3de656831e3b0e0e
xuansheep <xuansheep@gmail.com>
2021-02-02 08:56:17 +0800

    版本管理

---
+++ src/components/custom/ArrayForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="background-div">
+        <div v-for="item in listData">
+            <Input>{{item.key}}</Input>
+            <Input>{{item.value}}</Input>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ArrayForm",
+        props: {
+            data: {
+                type: Array,
+                default: [
+                    {key: '', value: ''}
+                ]
+            },
+        },
+        data() {
+            return {
+                listData: [],
+            }
+        },
+        created() {
+            this.listData = this.data;
+        }
+    }
+</script>
+
+<style scoped>
+    .background-div {
+
+    }
+</style>
