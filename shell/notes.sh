2b618c0f2b94d63e51358157bd2ddfd567ac4af2
xuansheep <xuansheep@gmail.com>
2022-01-26 14:48:53 +0800

    fix

---
+++ shell/notes.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#这里替换为你自己的执行程序(jar包)存放路径
+APP_PATH=/root/server/project/notes
+
+#代码仓库目录
+REPO_PATH=/root/server/repo/notes
+
+
+#输出带颜色文字
+warn() {
+    echo -e "\033[33m"$1"  \033[0m"
+}
+error(){
+        echo -e "\033[31m"$1"  \033[0m"
+}
+success(){
+        echo -e "\033[32m"$1"  \033[0m"
+}
+
+#使用说明，用来提示输入参数
+usage() {
+    echo "Usage: sh notes.sh [deploy]"
+    exit 1
+}
+
+#更新代码
+pull() {
+    cd $REPO_PATH
+    success "|---> git pull..."
+    git pull
+}
+
+
+# 编译打包
+package() {
+    cd $REPO_PATH
+    success "|---> install..."
+    npm install
+    success "|---> run build..."
+    npm run build
+    cp -rf $REPO_PATH/webroot $APP_PATH
+    success "|---> deploy finished"
+}
+
+#发布
+deploy() {
+    pull
+    package
+}
+
+
+#根据输入参数，选择执行对应方法，不输入则执行使用说明
+case "$1" in
+   "deploy")
+     deploy
+     ;;
+   *)
+     usage
+     ;;
+esac
