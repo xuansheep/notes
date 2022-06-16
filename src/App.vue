81034b475a9d18f8ddafc3d29465b7cd9e2f96ca
xuansheep <xuansheep@gmail.com>
2022-06-16 17:45:20 +0800

    live2d

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <!--<img alt="Vue logo" src="./assets/logo.png">-->
-      <router-view/>
-  </div>
+    <div id="app">
+        <!--<img alt="Vue logo" src="./assets/logo.png">-->
+        <router-view/>
+    </div>
 </template>
 
 <script>
